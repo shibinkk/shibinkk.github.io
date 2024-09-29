541f39fcfc643b6b590a0b3e2934c27c98e9409f
SHIBIN K K <shibinkk17@gmail.com>
2024-09-29 14:35:29 +0530

    Added email functionality using EmailJS

---
+++ assets/js/email.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_z6bq74e","template_72i0yol",parms).then(alert("Email Sent!!!"))
+}
